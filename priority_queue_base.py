4a47fa57201aeb97ee6136d0781b3f319523f8c8
matteogerlotti <matteo.gerlotti@edu.unifi.it>
2025-11-21 23:59:34 +0100

    """triple virgolette""" -> Python Docstring (commenti come TESTO DESCRITTIVO UFFICIALE) per documentazione, lette da help() ...Sphinx, IDE

--- priority_queue_base.py
+++ priority_queue_base.py
@@ -2,21 +2,40 @@
 
 class PriorityQueue:
     """
-    Interfaccia comune per tutte le implementazioni
-    di code di priorità nel progetto.
+    Classe base (interfaccia astratta) per tutte le implementazioni
+    di una coda di priorità.
+
+    Non contiene logica, ma definisce i metodi che tutte le
+    implementazioni concrete devono avere.
+    In Python non abbiamo interfacce vere, ma usiamo questa tecnica.
     """
+
     def insert(self, key):
-        """Inserisce un elemento con valore 'key'."""
+        """
+        Inserisce un elemento con valore 'key' all'interno della coda di priorità.
+
+        Metodo astratto: deve essere implementato nelle sottoclassi.
+        """
         raise NotImplementedError("Metodo non implementato")
 
     def extract_max(self):
-        """Rimuove e restituisce il valore massimo."""
+        """
+        Rimuove e restituisce il valore massimo contenuto nella coda di priorità.
+
+        Metodo astratto: deve essere implementato nelle sottoclassi.
+        """
         raise NotImplementedError("Metodo non implementato")
 
     def peek(self):
-        """Restituisce il massimo senza rimuoverlo."""
+        """
+        Restituisce il massimo senza rimuoverlo dalla struttura.
+
+        Utile per controllare l'elemento prioritario corrente.
+        """
         raise NotImplementedError("Metodo non implementato")
 
     def size(self):
-        """Numero di elementi nella struttura."""
+        """
+        Restituisce il numero di elementi attualmente presenti nella struttura.
+        """
         raise NotImplementedError("Metodo non implementato")
